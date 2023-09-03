937ec76a83e3cc5f756773fb6fee342a49f31b88
quinise <quinise.ercolano@gmail.com>
2023-09-03 12:14:21 -0700

    added tailwind css to skeleton site

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,13 +6,17 @@ const Login = () => {
 
     return (
         <div className="card">
-            {user && <h2>Welcome, {user.displayName}</h2>}
+            {user && <h2 className="text-3xl flex items-center justify-center">Welcome, {user.displayName}!</h2>}
             {error && <h2>Error: {String(error)}</h2>}
-            {loading && <h2>Loading...</h2>}
+            {loading && <h2 className="text-2xl">Loading...</h2>}
             {!user && !loading && !error && (
             <>
-                <h2>Login to MobileObi</h2>
-                <button onClick={signInWithGoogle}>Sign in with Google</button>
+            <h1 className='text-7xl mt-10 mb-10 flex items-center justify-center'>Mobile Obi</h1>
+            <img className="object-contain h-48 w-96 mb-20 mx-auto block" src="src/assets/ObiLogo.png"/>            
+            <h2 className='text-3xl mb-2 flex items-center justify-center'>Login to MobileObi</h2>
+            <div className='mt-5 mb-10 flex items-center justify-center'>
+                <button className='bg-blue-600/90 px-5 py-5' onClick={signInWithGoogle}><img className="object-cover h-10 w-10 mr-4 inline" src="src/assets/GoogleIcon.png"/><p className="text-white font-bold inline">Sign in with Google</p></button>
+            </div>
             </>
             )}
         </div>
