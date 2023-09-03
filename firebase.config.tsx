e64d983d2c11eb921aad97877be820165b26e7a0
quinise <quinise.ercolano@gmail.com>
2023-09-03 06:19:08 -0700

    removed firebase.config

--- firebase.config.tsx
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBM5fBNtyouQb9pojrIezhXsrTz01LoR2A",
-    authDomain: "obi-project-397801.firebaseapp.com",
-    projectId: "obi-project-397801",
-    storageBucket: "obi-project-397801.appspot.com",
-    messagingSenderId: "822880442553",
-    appId: "1:822880442553:web:a7f7d02b4fe2a6a4a7a368"
-  };
