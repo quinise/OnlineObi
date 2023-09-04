0f30bab373f5d162909c4b8e1347c0bad60ad623
quinise <quinise.ercolano@gmail.com>
2023-09-04 14:45:03 -0700

    added custom interface Cast to store user input and hold static cast data

---
+++ src/Interfaces/BaseCastArray.tsx
@@ -0,0 +1,201 @@
+// This file contains the different permutations of divination as an array of Casts
+
+import { Cast } from "./Cast";
+
+const aalaffiaOgbe: Cast = {
+    id: "0",
+    odu: "Aalaffia - Ogbe", 
+    timestamp: new Date(), 
+    answer: "Yes", 
+    maleObi1: "MaleObi1Up", 
+    maleObi2: "MaleObi2Up", 
+    femaleObi1: "FemaleObi1Up", 
+    femaleObi2: "FemaleObi2Up", 
+    interpretation: "Symbolizes good general welfare", 
+    title: ""
+}
+
+const yekuOyeku: Cast = {
+    id: "1",
+    odu: "Yeku - Oyeku", 
+    timestamp: new Date(), 
+    answer: "No", 
+    maleObi1: "MaleObi1Down", 
+    maleObi2: "MaleObi2Down", 
+    femaleObi1: "FemaleObi1Down", 
+    femaleObi2: "FemaleObi2Down", 
+    interpretation: "No light - potential delays and possible protection from ancestors...", 
+    title: ""
+}
+
+const okanranIleraOne: Cast = {
+    id: "2",
+    odu: "Okanran - Ilera", 
+    timestamp: new Date(), 
+    answer: "Maybe", 
+    maleObi1: "MaleObi1Up", 
+    maleObi2: "MaleObi2Down", 
+    femaleObi1: "FemaleObi1Down",
+    femaleObi2: "FemaleObi2Down", 
+    interpretation: "Good health and success!", 
+    title: ""
+}
+
+const okanranIleraTwo: Cast = {
+    id: "3",
+    odu: "Okanran - Ilera", 
+    timestamp: new Date(), 
+    answer: "Maybe", 
+    maleObi1: "MaleObi1Down", 
+    maleObi2: "MaleObi2Up", 
+    femaleObi1: "FemaleObi1Down", 
+    femaleObi2: "FemaleObi2Down", 
+    interpretation: "Good health and success!", 
+    title: ""
+}
+
+const okanranAjeOne: Cast = {
+    id: "4",
+    odu: "Okanran-Aje", 
+    timestamp: new Date(), 
+    answer: "No", 
+    maleObi1: "MaleObi1Down", 
+    maleObi2: "MaleObi2Up", 
+    femaleObi1: "FemaleObi1Down", 
+    femaleObi2: "FemaleObi2Down", 
+    interpretation: "Abundance", 
+    title: ""
+
+}
+
+const okanranAjeTwo: Cast = {
+    id: "5",
+    odu: "Okanran-Aje", 
+    timestamp: new Date(), 
+    answer: "No", 
+    maleObi1: "Male.Obi1Up", 
+    maleObi2: "MaleObi2Down", 
+    femaleObi1: "FemaleObi1Down", 
+    femaleObi2: "FemaleObi2Down", 
+    interpretation: "Abundance", 
+    title: ""
+}
+const okanranAjeThree: Cast = {
+    id: "6",
+    odu: "Okanran-Aje", 
+    timestamp: new Date(), 
+    answer: "No", 
+    maleObi1: "MaleObi1Down", 
+    maleObi2: "MaleObi2Down", 
+    femaleObi1: "FemaleObi1Up", 
+    femaleObi2: "FemaleObi2Down", 
+    interpretation: "Abundance", 
+    title: ""
+}
+
+const okanranAjeFour: Cast = {
+    id: "7",
+    odu: "Okanran-Aje", 
+    timestamp: new Date(), 
+    answer: "No", 
+    maleObi1: "MaleObi1Down", 
+    maleObi2: "MaleObi2Down", 
+    femaleObi1: "FemaleObi1Down", 
+    femaleObi2: "FemaleObi2Up", 
+    interpretation: "Abundance", 
+    title: ""
+}
+
+const eifeEjire: Cast = {
+    id: "8",
+    odu: "Ejife-Ejire", 
+    timestamp: new Date(), 
+    answer: "Yes", 
+    maleObi1: "MaleObi1Down", 
+    maleObi2: "MaleObi2Down", 
+    femaleObi1: "FemaleObi1Up", 
+    femaleObi2: "FemaleObi2Up", 
+    interpretation: "criminality, arguments", 
+    title: ""
+}
+
+const ejifeEroOne: Cast = {
+    id: "9",
+    odu: "Ejife-Ero", 
+    timestamp: new Date(), 
+    answer: "Maybe", 
+    maleObi1: "MaleObi1Up", 
+    maleObi2: "MaleObi2Down", 
+    femaleObi1: "FemaleObi1Up", 
+    femaleObi2: "FemaleObi2Up", 
+    interpretation: "calmness and whole-mindedness", 
+    title: ""
+}
+
+const ejifeEroTwo: Cast = {
+    id: "10",
+    odu: "Ejife-Ero", 
+    timestamp: new Date(), 
+    answer: "Maybe", 
+    maleObi1: "MaleObi1Down", 
+    maleObi2: "MaleObi2Up", 
+    femaleObi1: "FemaleObi1Up", 
+    femaleObi2: "FemaleObi2Up", 
+    interpretation: "calmness and whole-mindedness", 
+    title: ""
+}
+
+const obitaEtawaOne: Cast = {
+    id: "11",
+    odu: "Obita-Etawa", 
+    timestamp: new Date(), 
+    answer: "Maybe", 
+    maleObi1: "MaleObi1Down", 
+    maleObi2: "MaleObi2Up", 
+    femaleObi1: "FemaleObi1Up", 
+    femaleObi2: "FemaleObi2Up", 
+    interpretation: "No arguments, living harmoniously", 
+    title: ""
+}
+
+const obitaEtawaTwo: Cast = {
+    id: "12",
+    odu: "Obita-Etawa", 
+    timestamp: new Date(), 
+    answer: "Maybe", 
+    maleObi1: "MaleObi1Up", 
+    maleObi2: "MaleObi2Down", 
+    femaleObi1: "FemaleObi1Up", 
+    femaleObi2: "FemaleObi2Up", 
+    interpretation: "No arguments, living harmoniously", 
+    title: ""
+}
+
+const akitaEtawaOne: Cast = {
+    id: "13",
+    odu: "Akita-Etawa", 
+    timestamp: new Date(), 
+    answer: "Maybe", 
+    maleObi1: "MaleObi1Up", 
+    maleObi2: "MaleObi2Up", 
+    femaleObi1: "FemaleObi1Down", 
+    femaleObi2: "FemaleObi2Up", 
+    interpretation: "Hard work is rewarded", 
+    title: ""
+}
+
+const akitaEtawaTwo: Cast = {
+    id: "14",
+    odu: "Akita-Etawa", 
+    timestamp: new Date(), 
+    answer: "Maybe", 
+    maleObi1: "MaleObi1Up", 
+    maleObi2: "MaleObi2Up", 
+    femaleObi1: "FemaleObi1Up", 
+    femaleObi2: "FemaleObi2Down", 
+    interpretation: "Hard work is rewarded", 
+    title: ""
+}
+
+export const BaseCastArray: [Cast, Cast, Cast, Cast, Cast, Cast, Cast, Cast, Cast, Cast, Cast, Cast, Cast, Cast, Cast, Cast] = [ aalaffiaOgbe, yekuOyeku, okanranIleraOne, okanranIleraTwo, okanranAjeOne, okanranAjeTwo,okanranAjeThree, okanranAjeFour, eifeEjire, ejifeEroOne, ejifeEroTwo, obitaEtawaOne, obitaEtawaTwo, akitaEtawaTwo, akitaEtawaOne, akitaEtawaTwo];
+
