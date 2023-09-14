745942c8c1395646bacbc6601d44398ac4bf7b66
Devin Ercolano <devin.ercolano@gmail.com>
2023-09-14 16:40:21 -0700

    styled the Cast module that appears when a Cast div is clicked

--- src/components/CastList.tsx
+++ src/components/CastList.tsx
@@ -9,11 +9,23 @@ import Modal from "./Modal.tsx";
 
 const CastList = () => {
   const [user] = useAuthState(auth);
+  const [cast, setCast] = useState<Cast>({
+    id: makeid(8), 
+    odu:" Aalaffia - Ogbe",
+    timestamp: new Date(), 
+    answer: "Yes", 
+    maleObi1: "Male1Up.png", 
+    maleObi2: "Male2Up.png", 
+    femaleObi1: "Female1Up.png", 
+    femaleObi2: "Female2Up.png", 
+    interpretation: "Symbolizes good general welfare", 
+    title: ""
+    });
   const [casts, setCasts] = useState<Cast[]>([]);
   const [showModal, setShowModal] = useState(false);
   const castsRef = (collection(db, "casts"));
 
-  // Generate a random id to use for unique keys
+  // Generate a random id string to use for unique keys
   function makeid(length: number) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.?!@#$&*';
@@ -33,6 +45,27 @@ const CastList = () => {
     seen.add(cast.title);
     return !duplicate;
   })
+
+  function setModuleCast(inputCast: Cast) {
+     setCast(inputCast);
+     setShowModal(true)
+     return cast;
+  }
+
+  const renderListOfCasts = (uniqueCasts: Cast[]) => {
+    return (
+      <div>{uniqueCasts && uniqueCasts?.map((castFromList: Cast) =>
+        <div className="mt-16 mx-auto w-96 h-48 p-6 bg-lime-cream/40 border-2 border-forrest/60 rounded-tl-2xl shadow-md block" onClick={
+          () => setModuleCast(castFromList)}>
+          <div className="w-88 h-36 pt-12 pl-4 bg-lime-cream/60 rounded-lg border-2 border-forrest/40 flex justify-between">
+            <h1 className="text-2xl text-forrest inline" key={makeid(16)}>{castFromList.title}</h1>
+            <button className="bg-red text-white hover:bg-dark-red h-12 ml-0 mt-1 mb-1 mr-4 px-5 py-2 inline">Delete</button>
+          </div>
+        </div>
+        )}
+      </div>
+    );
+  }
 
   React.useEffect(() => {
     const fetchCasts  = () => {
@@ -64,29 +97,37 @@ const CastList = () => {
     };
 
     fetchCasts();
-    console.log(uniqueCasts);
 
   }, [castsRef, casts, uniqueCasts]);
 
   return (
     <Fragment>
       {user && <h2 className="text-3xl text-forrest mt-24 flex items-center justify-center">{user.displayName}'s Casts</h2>}
-      <div>{uniqueCasts && uniqueCasts?.map((cast) => 
-        <div className="mt-16 mx-auto w-96 h-48 p-6 bg-lime-cream/40 border-2 border-forrest/60 rounded-tl-2xl shadow-md block" onClick={() => setShowModal(true)}>
-          <div className="w-88 h-36 pt-12 pl-4 bg-lime-cream/60 rounded-lg border-2 border-forrest/40 flex justify-between">
-            <h1 className="text-2xl text-forrest inline" key={makeid(16)}>{cast.title}</h1>
-            <button className="bg-red text-white hover:bg-dark-red h-12 ml-0 mt-1 mb-1 mr-4 px-5 py-2 inline">Delete</button>
+      <div>
+        {renderListOfCasts(uniqueCasts)}
+      </div>
+        <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
+          <div className="p-6">
+            <h3 className="text-xl font-semibold text-forrest mb-5 flex justify-center">{cast.title}</h3>
+            <hr className="text-forrest rounded-lg md:w-[70%] md:mx-auto mb-8"/>
+           <div className="ml-12">
+            <p className="text-xl text-forrest mb-5 ">Odu: {cast.odu}</p>
+            <p className="text-xl text-forrest mb-5 ">Date: {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(cast.timestamp)}</p>
+            <p className="text-xl text-forrest mb-5 ">Answer: {cast.answer}</p>
+            <p className="text-xl text-forrest mb-5 ">Interpretation: {cast.interpretation}</p>
+           </div>
+            
+            <div className="mt-8 mb-8 flex justify-between">
+              <img className="object-scale-down h-32 w-32 inline" src={`src/assets/${cast.maleObi1}`}/>
+              <img className="object-scale-down h-32 w-32 inline" src={`src/assets/${cast.maleObi2}`}/>
+              <img className="object-scale-down h-32 w-32 inline" src={`src/assets/${cast.femaleObi1}`}/>
+              <img className="object-scale-down h-32 w-32 inline" src={`src/assets/${cast.femaleObi2}`}/>
+            </div>
+            <button className="bg-lime rounded-md text-ivory hover:bg-lime-cream h-12 ml-12 mt-1 mb-1 mr-4 px-5 py-2 inline">Edit</button>
           </div>
-        </div>)}
-      </div>
-    <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
-      <div className="p-6">
-        <h3 className="text-xl font-semibold text-forrest mb-5">{cast.title}</h3>
-      </div>
-    </Modal> 
+        </Modal>
     </Fragment>
   )
-
 }
 
 export default CastList;
