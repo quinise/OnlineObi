ab6c2389c70bedf137cef97ed5d86903362bf5e2
quinise <quinise.ercolano@gmail.com>
2023-09-18 05:55:10 -0700

    added navbar for other auth situations

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -17,7 +17,7 @@ const Login = () => {
             <img className="object-contain h-48 w-96 mb-20 mx-auto block" src="src/assets/ObiLogo.png"/>            
             <h2 className='text-3xl mb-2 flex items-center justify-center'>Login to MobileObi</h2>
             <div className='mt-5 mb-10 flex items-center justify-center'>
-                <button className='bg-blue-600/90 px-5 py-5' onClick={signInWithGoogle}><img className="object-cover h-10 w-10 mr-4 inline" src="src/assets/GoogleIcon.png"/><p className="text-white font-bold inline">Sign in with Google</p></button>
+                <button className='bg-googleBlue px-5 py-5' onClick={signInWithGoogle}><img className="object-cover h-10 w-10 mr-4 inline" src="src/assets/GoogleIcon.png"/><p className="text-white font-bold inline">Sign in with Google</p></button>
             </div>
             </>
             )}
