9e681bf6557453cc797e3a03680715d20d97379c
quinise <quinise.ercolano@gmail.com>
2023-10-02 06:26:53 -0700

    added animation to the web and mobile navigation

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -21,7 +21,11 @@ const Logout = () => {
     <>
       <h2 className='text-mahogany font-"sans-serif" mt-5 mb-10 flex items-center justify-center'>Are you sure you'd like to log out?</h2>
       <div className='mt-5 mb-10 flex items-center justify-center'>
-        <motion.button whileHover={{ scale: 1.1 }} className='bg-forrest rounded-md text-ivory font-"sans-serif" hover:bg-forrest/60 px-5 py-5' onClick={handleLogout}>Logout</motion.button>
+        <motion.button whileHover={{ 
+          scale: 1.1,
+          textShadow: "0px 0px 8px rgb(255, 255, 255)",
+          boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+        }} className='bg-forrest rounded-md text-ivory font-"sans-serif" hover:bg-forrest/60 px-5 py-5' onClick={handleLogout}>Logout</motion.button>
       </div>
     </>
   )
