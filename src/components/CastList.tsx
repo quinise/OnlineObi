f37cbfd082682268d13954e3bac2c4464830aa97
quinise <quinise.ercolano@gmail.com>
2023-10-07 14:28:24 -0700

    made the save button in Edit Cast appear depending on whether or not Edit Cast was clicked. Also added text input to Edit Cast title

--- src/components/CastList.tsx
+++ src/components/CastList.tsx
@@ -5,10 +5,10 @@ import { motion } from "framer-motion";
 import { Cast } from "../interfaces/Cast.tsx";
 import { fetchCasts } from "../services/fetchCasts.tsx";
 import { handleDelete } from "../services/deleteCast.tsx";
+// import EditCast from "../components/EditCast.tsx";
 import { auth } from "./../../GoogleProvider.tsx";
 import { uid } from "uid";
 import Modal from "./Modal.tsx";
-import EditCast from "./EditCast.tsx";
 
 // Provides the "bump" animation on hover
 const bumpVariants = {
@@ -37,6 +37,8 @@ const kolaVariants = {
 const CastList = () => {
   const [user] = useAuthState(auth);
   const [casts, setCasts] = useState<Cast[]>([]);
+  const [newTitle, setNewTitle] = useState("");
+  const [show, setShow] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [cast, setCast] = useState<Cast>({
     id: uid(), 
@@ -51,10 +53,21 @@ const CastList = () => {
     title: ""
   });
 
+  const handleEdit = (castFromList: Cast, editedTitle: string) => {
+    setNewTitle(editedTitle);
+    setShowModal(true);
+    setShow(true);
+  }
+
+  const handleUpdate = async (cast: Cast) => {
+
+  }
+
   // Provide the Module component with the selected cast
   function setModuleCast(inputCast: Cast) {
      setCast(inputCast);
      setShowModal(true)
+     setShow(false);
      return cast;
   }
 
@@ -73,9 +86,10 @@ const CastList = () => {
             </div>
           </motion.div>
         </div>
-        <span className="xl:ml-[0%] md:ml-[12%]">
-          <EditCast />
-        </span>
+        <motion.button className="bg-forrest rounded-md text-ivory font-sans-serif hover:bg-forrest/60 h-12 ml-[32%] xl:ml-[40%] mt-1 mb-1 mr-0 px-5 py-2 inline" key={`editButton${castFromList.id}`}
+          variants={bumpVariants}
+          whileHover="whileHover" onClick={() => handleEdit(castFromList, castFromList.title)}
+          >Edit Cast</motion.button>
         <motion.button className="bg-red text-white font-sans-serif hover:bg-darkRed h-12 px-5 py-2 ml-[18%] xl:ml-[9%]  mt-1 rounded-md" key={`deleteButton${castFromList.id}`} onClick={() => handleDelete(castFromList)}
           variants={bumpVariants}
           whileHover="whileHover"
@@ -88,7 +102,6 @@ const CastList = () => {
   }
 
   React.useEffect(() => {
-      
     fetchCasts((data: Cast[]) => {
       setCasts(data)
     });
@@ -102,10 +115,12 @@ const CastList = () => {
       </div>
         <Modal isVisible={showModal} onClose={() => setShowModal(false)}>
           <div className="p-6">
-            <motion.h3 className="text-xl font-semibold text-forrest font-serif mb-5 flex justify-center"
+          {show ? <input className="text-forrest font-sans-serif mb-2 flex mx-auto" placeholder={cast.title} value={newTitle} onChange={(e) => setNewTitle(e.target.value)}/>
+          : <motion.h3 className="text-xl font-semibold text-forrest font-serif mb-5 flex justify-center"
               variants={titleBumpVariants}
               whileHover="whileHover"
             >{cast.title}</motion.h3>
+            }
             <hr className="text-forrest rounded-lg md:w-[70%] md:mx-auto mb-8"/>
             <div className="ml-12">
               <p className="text-xl text-forrest font-sans-serif  mb-5 "><b>Odu:</b>  {cast.odu}</p>
@@ -131,6 +146,11 @@ const CastList = () => {
               whileHover="whileHover"
               src={`src/assets/${cast.femaleObi2}`}/>
             </div>
+            {show && <motion.button className="bg-forrest rounded-md text-ivory font-sans-serif hover:bg-forrest/60 h-12 ml-[32%] xl:ml-[40%] mt-1 mb-1 mr-0 px-5 py-2 inline" key={`editButton${cast.id}`}
+              variants={bumpVariants}
+              whileHover="whileHover" onClick={() => handleUpdate(cast)}
+              >Save</motion.button>
+            }
           </div>
         </Modal>
     </Fragment>
