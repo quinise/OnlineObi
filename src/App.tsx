6087f4cd7ace327d727d812868ee06d2cb1c7b0a
quinise <quinise.ercolano@gmail.com>
2025-11-24 18:16:42 -0800

    Added a footer to the bottom of the app

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import { auth } from "../firebase.config";
 import Auth from './components/Auth';
 import CastList from './components/CastList.tsx';
 import Dashboard from './components/Dashboard';
+import Footer from './components/Footer';
 import Loader from './components/Loader.tsx';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Navbar from './components/Navbar';
 import Settings from './components/Settings.tsx';
-import Footer from './components/Footer';
 import './css/App.css';
 
 function App() {
@@ -22,59 +22,64 @@ function App() {
 
   if (error) {
     console.log("useAuthState Error: " + error);
+    // No Navbar with useAuthState error, but a link to login instead
     return (
-      // No Navbar with useAuthState error, but a link to login instead
-      <div className='bg-ivory'>
-        <Routes>
-          <Route path="/" element={ <Login /> } /> 
-          <Route path="/login" element={ <Login /> } />
-        </Routes>
+      <div className='bg-ivory min-h-screen flex flex-col'>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={ <Login /> } /> 
+            <Route path="/login" element={ <Login /> } />
+          </Routes>
+        </main>
         <Footer />
       </div>
-  )}
-
+    );
+  }
   if (user) {
     return (
       // Navbar with a valid user, includes logout
-      <div className="bg-ivory h-screen">
-        <Settings />
-        <Navbar />
-        <div>
-          <Routes> 
-          <Route path="/" element={
-              <Auth>
-                <Dashboard />
-              </Auth>
-            } />
-            <Route path="/dashboard" element={
-              <Auth>
-                <Dashboard />
-              </Auth>
-            } />
-            <Route path="/castList" element={
-              <Auth>
-                <CastList />
-              </Auth>
-            } />
-            <Route path="/logout" element={
-              <Auth>
-                <Logout />
-              </Auth>
-            } />
-          </Routes>
-        </div>
+      <div className="bg-ivory min-h-screen flex flex-col">
+        <main className="flex-grow">
+          <Settings />
+          <Navbar />
+          <div>
+            <Routes> 
+              <Route path="/" element={
+                <Auth>
+                  <Dashboard />
+                </Auth>
+              } />
+              <Route path="/dashboard" element={
+                <Auth>
+                  <Dashboard />
+                </Auth>
+              } />
+              <Route path="/castList" element={
+                <Auth>
+                  <CastList />
+                </Auth>
+              } />
+              <Route path="/logout" element={
+                <Auth>
+                  <Logout />
+                </Auth>
+              } />
+            </Routes>
+          </div>
+        </main>
         <Footer />
       </div>
     );
   } else {
     // return the Login page if there is no user
     return (
-      <div className='bg-ivory h-screen'>
-        <Routes>
-          <Route path="/" element={ <Login /> } /> 
-          <Route path="/login" element={ <Login /> } 
-          />
-        </Routes>
+      <div className='bg-ivory min-h-screen flex flex-col'>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={ <Login /> } />
+            <Route path="/login" element={ <Login /> } />
+          </Routes>
+        </main>
         <Footer />
       </div>
     )
